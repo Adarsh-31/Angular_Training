d09381689b62afa2e1385be7de8b4112d3ed407b
Adarsh Goswami <61086546+Adarsh-31@users.noreply.github.com>
2022-12-21 12:52:43 +0530

    Add files via upload

---
+++ src_thrid/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'thirdque';
+  clicked:boolean=false;
+  count:number=0;
+  arr:Array<number>=[];
+  constructor()
+  {
+    this.clicked=false;
+  }
+  btnClicked()
+  {
+    this.count++;
+    this.arr.push(this.count);
+    if(this.count%2==0)
+    {
+    this.clicked=false;
+    }
+    else
+    {
+      this.clicked=true;
+    }
+  }
+}
