c78a0f117677b7e61bd85a955c52c839469bbfda
Adarsh Goswami <61086546+Adarsh-31@users.noreply.github.com>
2022-12-30 11:22:54 +0530

    Add files via upload

---
+++ src 28_12_22/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private fb:FormBuilder)
+  {
+
+  }
+  istrue:boolean=false
+  form =this.fb.group({
+    username:['',Validators.required,Validators.pattern("^[a-zA-Z0-9 ]*$")],
+    password: ['',Validators.required]
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.istrue=true
+     
+     
+    }
+   }
+  
+  
+}
